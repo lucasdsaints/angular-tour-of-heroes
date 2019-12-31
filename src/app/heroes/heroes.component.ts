1bd58f9d05976ce4a03d166a51fbdbe614d02aab
Lucas Santos <lucas.assis.santos96@gmail.com>
2019-12-31 09:58:14 -0300

    Concluido secao 3.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
